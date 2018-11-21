5f2aaa24d1d8c60f1a59236875cd3292acebde51
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2018-11-21 11:11:56 +0100

    just some unfinished styling; WIP

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,14 +11,34 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to list_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
+
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to list_path
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to list_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
